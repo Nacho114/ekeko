479f8fb1e95e87c1d66df03ad23c48090d0f7ff2
Nacho114 <17376073+Nacho114@users.noreply.github.com>
2024-09-06 22:20:34 +0200

    quotes: updated

--- ekeko/fun/quotes.py
+++ ekeko/fun/quotes.py
@@ -3,11 +3,14 @@ import random
 
 
 def print_random_quote():
-    quotes = [
-        "when an inner situation is not made conscious, it happens outside, as Fate. - CJ",
+    smart_quotes = [
+        "With four parameters I can fit an elephant, and with five I can make him wiggle his trunk. - Von Neumann",
+        "Some strategies work in some markets some of the time - Kedar",
+    ]
+    wise_quotes = [
+        "when an inner situation is not made conscious, it happens outside, as Fate. - C.G. Jung",
         "A fool who persists in his folly becomes wise (or rich?). - Blake",
         "There is a voice that does not use words. Listen. - Rumi",
-        "There is always something trending",
         "Nature does not hurry, yet everything is accomplished. - Lao Tzu",
         "Silence is the language of God, all else is poor translation. - Rumi",
         "The quieter you become, the more you can hear. - Ram Dass",
@@ -20,5 +23,8 @@ def print_random_quote():
         "古池や蛙飛こむ水のをと - 芭蕉",
         "Always remember that when you're pointing your finger at someone, you've got three pointing back at yourself. - Unknown",
         "You are special because of who you are - kedar",
+        "In good times, be conscious of the seed that will bring about bad times. When times are bad, acknowledge the seed that will bring change. - Nacho",
+        "一期一会 | one time, one meeting",
     ]
-    cowsay.cow(random.choice(quotes))
+    quotes = smart_quotes + wise_quotes
+    cowsay.cow(random.choice(quotes)) # type: ignore
