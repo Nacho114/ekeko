d41be34439a580a47f1600a81a32f8aaaef24663
Nacho114 <17376073+Nacho114@users.noreply.github.com>
2024-11-19 22:26:18 +0100

    yfinance: load max data, and the filter by years

--- ekeko/data_loader/data_loader.py
+++ ekeko/data_loader/data_loader.py
@@ -4,6 +4,8 @@ from ekeko.core.types import Ticker
 import pandas as pd
 import yfinance as yf
 
+from datetime import timedelta
+
 
 class DataLoader(Protocol):
 
@@ -14,26 +16,33 @@ class DataLoader(Protocol):
 
 class YfinanceDataLoader:
 
-    def __init__(self, period: str | list[str]):
-        if isinstance(period, str):
-            period = [period]
+    def __init__(self, period: str):
         self.period = period
 
     def load(self, ticker: Ticker) -> pd.DataFrame | None:
         stock = yf.Ticker(ticker)
 
-        for p in self.period:
-            stock_df = stock.history(period=p)
+        stock_df = stock.history(period='max')
 
-            if stock_df.empty:
-                return None
+        if stock_df.empty:
+            return None
 
-            has_nan = stock_df.isna().any().any() # type: ignore
-            if has_nan:
-                print(f'{ticker} has df, but with nan values.')
-                return None
+        has_nan = stock_df.isna().any().any() # type: ignore
+        if has_nan:
+            print(f'{ticker} has df, but with nan values.')
+            return None
 
-            return stock_df
+        # Calculate the cutoff date based on the period
+        recent_date = stock_df.index[-1]
+        cutoff_date = recent_date - timedelta(days=int(self.period[:-1]) * 365)
+
+        # Filter the DataFrame to keep rows from the cutoff date onward
+        filtered_df = stock_df[stock_df.index >= cutoff_date]        
+
+        if isinstance(filtered_df, pd.DataFrame):
+            return filtered_df
+    
+        return None
 
     def process(self, stock_df: pd.DataFrame) -> pd.DataFrame:
         return stock_df
